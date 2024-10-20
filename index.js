a8309e145a25691effcda72132c6a3e78742b7bb
Deepanshu Kumar <deepskmr822@gmail.com>
2024-10-20 21:41:50 +0530

    feat: Adding apis to get users and user by id

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const cors = require("cors");
+
+const PORT = 3000;
+
+const app = express();
+app.use(cors());
+
+const users = [
+  {
+    id: 1,
+    username: "octocat",
+    name: "The Octocat",
+    repoCount: 8,
+    location: "San Francisco",
+  },
+  {
+    id: 2,
+    username: "torvalds",
+    name: "Linus Torvalds",
+    repoCount: 25,
+    location: "Portland",
+  },
+  {
+    id: 3,
+    username: "gaearon",
+    name: "Dan Abramov",
+    repoCount: 50,
+    location: "London",
+  },
+  {
+    id: 4,
+    username: "addyosmani",
+    name: "Addy Osmani",
+    repoCount: 42,
+    location: "Mountain View",
+  },
+  {
+    id: 5,
+    username: "tj",
+    name: "TJ Holowaychuk",
+    repoCount: 150,
+    location: "Victoria",
+  },
+];
+
+app.get("/users", (req, res) => {
+  res.json({ users });
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  if (user) {
+    res.status(200).json({ user });
+  } else {
+    return res.status(404).json({ message: "User not found" });
+  }
+});
+
+app.listen(PORT, () => console.log(`Server started listening on ${PORT} port`));
